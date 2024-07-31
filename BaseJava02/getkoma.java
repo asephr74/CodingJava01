939a6227510b3bec32299962534946fcc9455eac
asephr74 <147579601+asephr74@users.noreply.github.com>
2024-07-31 09:10:07 +0700

    Update getkoma.java

--- BaseJava02/getkoma.java
+++ BaseJava02/getkoma.java
@@ -8,16 +8,16 @@ public class Main {
         // Tambahkan koma di akhir string
         sString = sString + ",";
 
-        for (int iI = 0; iI < iLen; iI++) {
+        for (int iI = 0; iI < iLen; iI++) { //25x looping
             char sKoma = sString.charAt(iI);
             if (sKoma == ',') {
-                iFound++;
+                iFound++; //nambah 2x
                 if (iFound == iCounter) {
                     return sTemp;
                 }
-                sTemp = "";
-            } else {
-                sTemp += sKoma;
+                sTemp = ""; // inisialisasi lgi  ke "" jika tidak sama dgn 2 ,
+            } else {// jika tidak menemukan ,
+                sTemp += sKoma;// stemp=stemp+koma=>stemp ke isi nilai string=>(apple,banana,orange,grape)                      
             }
         }
         return sTemp;
@@ -26,6 +26,6 @@ public class Main {
     public static void main(String[] args) {
         String sString = "apple,banana,orange,grape";
         int iCounter = 4;
-        System.out.println("String ke-" + iCounter + " dari '" + sString + "' adalah: " + getkoma(sString, iCounter));
+        System.out.println("String ke-" + iCounter + " dari '" + sString + "' adalah: " + getkoma(sString, iCounter)); //isinya 'banana'
     }
 }
